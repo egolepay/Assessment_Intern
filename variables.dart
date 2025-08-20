662012ca68fe85b979ce2aaa5bfecd94502bdc57
unknown <olumuyiwaalade2004@gmail.com>
2025-08-20 21:11:17 +0100

    Add variables files

---
+++ variables.dart
@@ -0,0 +1,36 @@
+void main() {
+  // Integer
+  //int age = 25;
+
+  // Double
+  //double height = 5.9;
+
+  // String
+  //String name = "Ahmad";
+
+  // Boolean
+  //bool isStudent = true;
+
+  // List
+  //List<String> fruits = ['Apple', 'Banana', 'Orange'];
+
+  // Map
+  //Map<String, int> scores = {'Math': 90, 'Science': 85};
+
+  // Set
+  //Set<int> uniqueNumbers = {1, 2, 3, 4};
+
+  // Dynamic
+  //dynamic anything = "Can be any type";
+  //anything = 123; // Now it's an int
+
+  // var (type inferred)
+  //var city = "London";
+  //var number = 42;
+
+  // const (compile-time constant)
+  //const double pi = 3.14159;
+
+  // final (run-time constant)
+  //final DateTime now = DateTime.now();
+}
