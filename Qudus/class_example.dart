cba83d67b33f3e12cfdfcb5d003eac17a1b7cb08
Qudus Adeboye <131000916+Qudus14@users.noreply.github.com>
2025-08-21 11:38:35 +0100

    Update class_example.dart

---
+++ Qudus/class_example.dart
@@ -0,0 +1,11 @@
+class Car {
+  String brand;
+  String model;
+  int year;
+
+  Car(this.brand, this.model, this.year);
+
+  void displayInfo() {
+    print('Brand: $brand, Model: $model, Year: $year');
+  }
+}
